a5a6c471e6316702ed4e11d0bff686c5f0c1c0fb
atayab3 <59861431+atayab3@users.noreply.github.com>
2020-05-02 12:31:01 -0500

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,7 @@
 //
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then((registration)=> {
+    navigator.serviceWorker.register('/serviceworker.js').then((registration)=> {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, (err)=> {
