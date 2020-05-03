1aa52727ebfd3e2cc4fe130d31cb68ad892bb458
atayab3 <59861431+atayab3@users.noreply.github.com>
2020-05-03 00:13:30 -0500

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,6 @@
 //Followed Hayes Class Tutorial on 04/08/2020
 //
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/serviceworker.js').then((registration)=> {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (err)=> {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+
 
 
 var CACHE_NAME = 'nba-app-v2';
